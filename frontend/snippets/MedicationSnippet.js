5b5a40d64daa932e103e79e6cfb08108e8b6e089
JarnoPauwels <131155199+JRNPWL@users.noreply.github.com>
2024-06-10 09:16:02 +0200

    Added Custom Icons + Fixed displaying of Time

--- frontend/snippets/MedicationSnippet.js
+++ frontend/snippets/MedicationSnippet.js
@@ -1,13 +1,22 @@
 // MedicationSnippet.js
 
 import React from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faPlus, faPills } from "@fortawesome/free-solid-svg-icons";
 
 const MedicationSnippet = ({ navigation, medications }) => {
   const navigateToMedicationScreen = () => {
     navigation.navigate("MedicationScreen");
+  };
+
+  const Icon = {
+    Pill1: require("../assets/Pill1.png"),
+    Pill2: require("../assets/Pill2.png"),
+    Pill3: require("../assets/Pill3.png"),
+    Pill4: require("../assets/Pill4.png"),
+    Pill5: require("../assets/Pill5.png"),
+    Pill6: require("../assets/Pill6.png"),
   };
 
   return (
@@ -23,14 +32,27 @@ const MedicationSnippet = ({ navigation, medications }) => {
             <View key={index} style={styles.medicationItem}>
               <View style={styles.infoContainer}>
                 <View style={styles.iconContainer}>
-                  <FontAwesomeIcon icon={faPills} size={64} color="black" />
+                  {/* <FontAwesomeIcon icon={faPills} size={64} color="black" /> */}
+                  <Image
+                    source={Icon[medicationData.icon]}
+                    style={styles.icon}
+                  />
                 </View>
                 <View style={styles.detailsContainer}>
                   <Text style={styles.medName}>{medicationData.name}</Text>
                   <View style={styles.infoBottomContainer}>
                     <Text style={styles.text}>{medicationData.dosage}, </Text>
                     {/* <Text style={styles.text}>{medicationData.frequency}, </Text> */}
-                    <Text style={styles.text}>{medicationData.time}</Text>
+                    <Text style={styles.text}>
+                      {new Date(medicationData.time).toLocaleTimeString(
+                        "en-US",
+                        {
+                          hour12: false,
+                          hour: "2-digit",
+                          minute: "2-digit",
+                        }
+                      )}
+                    </Text>
                   </View>
                 </View>
               </View>
@@ -90,6 +112,10 @@ const styles = StyleSheet.create({
     width: "45%",
     alignItems: "center",
     justifyContent: "center",
+  },
+  icon: {
+    width: 64,
+    height: 64,
   },
   detailsContainer: {
     width: "55%",
