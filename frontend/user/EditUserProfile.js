1da6e99f632dd7e86109b853cfd73faac82cebc0
JarnoPauwels <131155199+JRNPWL@users.noreply.github.com>
2024-06-14 16:03:07 +0200

    Expanded Edit Profile
    - Changed picking Sex to Buttons instead of dropdown
    - Added Dropdown for picking bloodtype
    - Added Undo function to Medical History
    - Added Code for Updating the Profile Picture

--- frontend/user/EditUserProfile.js
+++ frontend/user/EditUserProfile.js
@@ -8,6 +8,7 @@ import {
   ScrollView,
   TouchableOpacity,
   Button,
+  Image,
 } from "react-native";
 import { getUserId, editProfile } from "../services/authService";
 import { useNavigation } from "@react-navigation/native";
@@ -17,11 +18,18 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { fetchUserData } from "../services/dataService";
 import DateTimePickerModal from "react-native-modal-datetime-picker";
 import { Picker } from "@react-native-picker/picker";
+import * as ImagePicker from "expo-image-picker";
+import "react-native-get-random-values";
+import { v4 as uuidv4 } from "uuid";
 
 const EditUserProfile = () => {
   const navigation = useNavigation();
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(false);
+
+  const [profilePicture, setProfilePicture] = useState(null);
+  const [profilePictureId, setProfilePictureId] = useState(null);
+  const [profilePictureType, setProfilePictureType] = useState(null);
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -36,6 +44,37 @@ const EditUserProfile = () => {
   const [error, setError] = useState(null);
   const [selectedDate, setSelectedDate] = useState("");
   const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+
+  const bloodTypes = ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"];
+
+  const handleChooseProfilePicture = async () => {
+    try {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        allowsEditing: true,
+        aspect: [4, 3],
+        quality: 1,
+        base64: true,
+      });
+
+      // console.log(result);
+      if (!result.canceled) {
+        const { base64, uri, mimeType } = result;
+        const uuid = uuidv4(); // Generate UUID
+        // const fileName = uri.split("/").pop(); // Extract filename from uri
+
+        // console.log(uuid);
+        console.log(result);
+        console.log(result.assets[0].uri);
+
+        // setProfilePictureId(uuid); // Set UUID as profilePictureId
+        setProfilePicture(result);
+        // setProfilePictureType(result.assets[0].mimeType); // Set image type
+      }
+    } catch (error) {
+      console.error("Error picking image:", error);
+    }
+  };
 
   const showDatePicker = () => {
     setDatePickerVisibility(true);
@@ -56,6 +95,14 @@ const EditUserProfile = () => {
       try {
         const user = await fetchUserData();
         setUserData(user);
+        setName(user.name);
+        setEmail(user.email);
+        setSex(user.sex);
+        setDob(user.dob);
+        setBloodType(user.bloodType);
+        setDoctor(user.doctor);
+        setEmergencyContact(user.emergencyContact);
+        setMedicalHistory(user.medicalHistory || []);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching user:", error);
@@ -72,10 +119,20 @@ const EditUserProfile = () => {
     }
   };
 
+  const [removedItems, setRemovedItems] = useState([]);
+
   const removeMedicalHistoryItem = (index) => {
     const updatedMedicalHistory = [...medicalHistory];
-    updatedMedicalHistory.splice(index, 1);
+    const removedItem = updatedMedicalHistory.splice(index, 1);
     setMedicalHistory(updatedMedicalHistory);
+    setRemovedItems([...removedItems, removedItem[0]]);
+  };
+
+  const undoRemoveMedicalHistoryItem = (index) => {
+    const updatedRemovedItems = [...removedItems];
+    const restoredItem = updatedRemovedItems.splice(index, 1);
+    setRemovedItems(updatedRemovedItems);
+    setMedicalHistory([...medicalHistory, restoredItem[0]]);
   };
 
   const editProfile = async () => {
@@ -85,33 +142,20 @@ const EditUserProfile = () => {
 
       const apiUrl = `http://192.168.0.119:3000/api/users/${userId}`;
 
-      const fields = [
-        { field: "name", value: name },
-        { field: "email", value: email },
-        { field: "password", value: password },
-        {
-          field: "dob",
-          value: selectedDate ? selectedDate.toISOString().split("T")[0] : null,
-        },
-        { field: "sex", value: sex },
-        { field: "bloodType", value: bloodType },
-        { field: "doctor", value: doctor },
-        { field: "emergencyContact", value: emergencyContact },
-        {
-          field: "medicalHistory",
-          value: medicalHistory.length > 0 ? medicalHistory : null,
-        },
-      ];
-
-      const putData = fields.reduce((acc, { field, value }) => {
-        if (
-          (value && typeof value === "string" && value.trim() !== "") ||
-          Array.isArray(value)
-        ) {
-          acc[field] = value;
-        }
-        return acc;
-      }, {});
+      // Construct the putData object directly
+      const putData = {
+        // profilePictureId,
+        profilePicture,
+        // profilePictureType,
+        name,
+        email,
+        dob: selectedDate ? selectedDate.toISOString().split("T")[0] : null,
+        sex,
+        bloodType,
+        doctor,
+        emergencyContact,
+        medicalHistory,
+      };
 
       console.log("putData:", putData); // Log the final putData object
 
@@ -148,6 +192,16 @@ const EditUserProfile = () => {
           <Text style={styles.title}>Update Profile</Text>
 
           <View style={styles.personalInfoContainer}>
+            <TouchableOpacity onPress={handleChooseProfilePicture}>
+              <Text>Choose Profile Picture</Text>
+            </TouchableOpacity>
+            {profilePicture ? (
+              <Image
+                source={{ uri: profilePicture }}
+                style={{ width: 100, height: 100 }}
+              />
+            ) : null}
+
             <Text style={styles.subTitle}>Personal Info</Text>
             <View style={styles.inputTitleContainer}>
               <Text style={styles.label}>Name</Text>
@@ -169,10 +223,7 @@ const EditUserProfile = () => {
             </View>
             <View style={styles.inputTitleContainer}>
               <Text style={styles.label}>Date Of Birth (DOB)</Text>
-              {/* <Button title="Show Date Picker" onPress={showDatePicker} /> */}
-              <Text
-              // style={{ fontSize: 24, fontWeight: "bold", marginBottom: 20 }}
-              >
+              <Text style={styles.dateText}>
                 {selectedDate
                   ? selectedDate.toLocaleDateString()
                   : "No date selected"}
@@ -181,10 +232,9 @@ const EditUserProfile = () => {
                 style={styles.dateTimePicker}
                 onPress={showDatePicker}
               >
-                <Text style={styles.dateTimePickerText}>Open Date Picker</Text>
+                <Text style={styles.dateTimePickerText}>Open Calander</Text>
               </TouchableOpacity>
               <DateTimePickerModal
-                // date={selectedDate}
                 isVisible={isDatePickerVisible}
                 mode="date"
                 onConfirm={handleConfirm}
@@ -193,27 +243,56 @@ const EditUserProfile = () => {
             </View>
             <View style={styles.inputTitleContainer}>
               <Text style={styles.label}>Sex</Text>
-              <Picker
-                selectedValue={sex}
-                style={styles.input}
-                onValueChange={(itemValue) => setSex(itemValue)}
-              >
-                <Picker.Item label="Select Sex" value="" />
-                <Picker.Item label="Male" value="M" />
-                <Picker.Item label="Female" value="F" />
-              </Picker>
+              <View style={styles.sexContainer}>
+                <TouchableOpacity
+                  style={[
+                    styles.sexButton,
+                    sex === "M" && styles.selectedSexButton,
+                  ]}
+                  onPress={() => setSex("M")}
+                >
+                  <Text
+                    style={[
+                      styles.sexButtonText,
+                      sex === "M" && { color: "white" },
+                    ]}
+                  >
+                    Male
+                  </Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[
+                    styles.sexButton,
+                    sex === "F" && styles.selectedSexButton,
+                  ]}
+                  onPress={() => setSex("F")}
+                >
+                  <Text
+                    style={[
+                      styles.sexButtonText,
+                      sex === "F" && { color: "white" },
+                    ]}
+                  >
+                    Female
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
           <View style={styles.medicalInfoContainer}>
             <Text style={styles.subTitle}>Medical Info</Text>
             <View style={styles.inputTitleContainer}>
               <Text style={styles.label}>Blood Type</Text>
-              <TextInput
-                placeholder={userData?.bloodType || "Blood Type"}
-                value={bloodType}
-                onChangeText={setBloodType}
+              <Picker
+                selectedValue={bloodType}
                 style={styles.input}
-              />
+                onValueChange={(itemValue) => setBloodType(itemValue)}
+              >
+                <Picker.Item label="Select Blood Type" value="" />
+                {bloodTypes.map((type) => (
+                  <Picker.Item label={type} value={type} key={type} />
+                ))}
+              </Picker>
             </View>
             <View style={styles.inputTitleContainer}>
               <Text style={styles.label}>Doctor</Text>
@@ -232,6 +311,7 @@ const EditUserProfile = () => {
                 onChangeText={setEmergencyContact}
                 style={styles.input}
                 keyboardType="phone-pad"
+                maxLength={10}
               />
             </View>
             <View style={styles.inputTitleContainer}>
@@ -239,16 +319,27 @@ const EditUserProfile = () => {
               <View>
                 {medicalHistory.map((item, index) => (
                   <View key={index} style={styles.medicalHistoryItem}>
-                    <Text>{item}</Text>
-                    {/* <TouchableOpacity
-                      title="Remove"
-                      onPress={() => removeMedicalHistoryItem(index)}
-                    /> */}
+                    <Text style={styles.medicalHistoryText}>{item}</Text>
                     <TouchableOpacity
-                      style={styles.dateTimePicker}
+                      style={styles.undoButton}
                       onPress={() => removeMedicalHistoryItem(index)}
                     >
-                      <Text style={styles.dateTimePickerText}>Remove</Text>
+                      <Text style={styles.buttonText}>Remove</Text>
+                    </TouchableOpacity>
+                  </View>
+                ))}
+                {removedItems.map((item, index) => (
+                  <View key={index} style={styles.medicalHistoryItem}>
+                    <Text
+                      style={[styles.medicalHistoryText, styles.removedText]}
+                    >
+                      {item}
+                    </Text>
+                    <TouchableOpacity
+                      style={styles.undoButton}
+                      onPress={() => undoRemoveMedicalHistoryItem(index)}
+                    >
+                      <Text style={styles.buttonText}>Undo</Text>
                     </TouchableOpacity>
                   </View>
                 ))}
@@ -258,7 +349,6 @@ const EditUserProfile = () => {
                   onChangeText={setNewMedicalHistoryItem}
                   style={styles.input}
                 />
-                {/* <TouchableOpacity title="Add" onPress={addMedicalHistoryItem} /> */}
                 <TouchableOpacity
                   style={styles.dateTimePicker}
                   onPress={addMedicalHistoryItem}
@@ -285,7 +375,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     backgroundColor: "white",
-    paddingBottom: 60, // Keep same number as footer+20
+    paddingBottom: 60,
   },
   scrollContainer: {
     width: "100%",
@@ -294,7 +384,7 @@ const styles = StyleSheet.create({
     width: "100%",
     marginTop: 50,
     paddingHorizontal: 20,
-    paddingBottom: 40, // Keep same number as footer+20
+    paddingBottom: 40,
   },
   personalInfoContainer: {
     marginBottom: 20,
@@ -326,6 +416,13 @@ const styles = StyleSheet.create({
     alignSelf: "flex-start",
     marginLeft: 10,
   },
+  dateText: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+    alignSelf: "flex-start",
+    marginLeft: 10,
+  },
   input: {
     borderWidth: 1,
     borderColor: "lightgrey",
@@ -333,14 +430,6 @@ const styles = StyleSheet.create({
     padding: 10,
     marginBottom: 10,
     width: "100%",
-    // shadowColor: "#000",
-    // shadowOffset: {
-    //   width: 2,
-    //   height: 3,
-    // },
-    // shadowOpacity: 0.25,
-    // shadowRadius: 5,
-    // elevation: 6,
   },
   dateTimePicker: {
     alignItems: "center",
@@ -351,27 +440,60 @@ const styles = StyleSheet.create({
     borderRadius: 15,
     paddingTop: 6,
     paddingBottom: 6,
-    // shadowColor: "#000",
-    // shadowOffset: {
-    //   width: 2,
-    //   height: 3,
-    // },
-    // shadowOpacity: 0.25,
-    // shadowRadius: 5,
-    // elevation: 6,
     marginBottom: 10,
   },
   dateTimePickerText: {
     color: "black",
     fontSize: 17,
-    // fontWeight: "bold",
+    fontWeight: "500",
+  },
+  sexContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
+  },
+  sexButton: {
+    alignItems: "center",
+    width: "49%",
+    borderWidth: 1,
+    borderColor: "lightgrey",
+    borderRadius: 25,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    marginBottom: 10,
+  },
+  selectedSexButton: {
+    alignItems: "center",
+    backgroundColor: "#4facfe",
+    borderColor: "#4facfe",
+  },
+  sexButtonText: {
+    color: "black",
+    fontSize: 20,
     fontWeight: "500",
   },
   medicalHistoryItem: {
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
     marginBottom: 10,
+  },
+  medicalHistoryText: {
+    flex: 1,
+    marginLeft: 10,
+  },
+  removedText: {
+    color: "red",
+    textDecorationLine: "line-through",
+  },
+  undoButton: {
+    backgroundColor: "#4facfe",
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: "white",
   },
   buttonContainer: {
     width: "100%",
